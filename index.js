99cab414016a5dd0041381c05b819c84a99c1e88
Ariel Zhen <zhenpingping@live.com>
2018-04-27 04:41:21 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,15 @@
+var katzDeliLine = [];
+
+function takeANumber(katzDeliLine, name){
+  katzDeliLine.push(name);
+  var number = katzDeliLine.indexof(name) + 1;
+  return "Welcome, ${name}. You are number ${number} in line."
+}
+
+function nowServing(){
+  
+}
+
+function currentLine(){
+  
+}
