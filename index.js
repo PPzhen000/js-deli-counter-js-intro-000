5275d42960dbbd698dc63d11d76ed617ed9cec3f
Ariel Zhen <zhenpingping@live.com>
2018-04-27 04:44:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var katzDeliLine = [];
 
 function takeANumber(katzDeliLine, name){
   katzDeliLine.push(name);
-  var number = katzDeliLine.indexof(name) + 1;
+  var number = katzDeliLine.indexOf(name) + 1;
   return "Welcome, ${name}. You are number ${number} in line."
 }
 
