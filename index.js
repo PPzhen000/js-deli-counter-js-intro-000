6fa53202fb242402b41de28b102889c38cb3a948
Ariel Zhen <zhenpingping@live.com>
2018-04-27 04:51:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ function nowServing(katzDeliLine){
   if(katzDeliLine.length === 0){
     return "There is nobody waiting to be served!";
   }
+  delete katzDeliLine[0];
   return `Currently serving ${katzDeliLine[0]}.`;
 }
 
